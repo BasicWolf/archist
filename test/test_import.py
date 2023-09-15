27412d0b26442f7335e168f63bc8977978b77667
Zaur Nasibov <zaur@znasibov.info>
2023-09-15 23:28:35 +0300

    Temporary remove ModuleScanner
    It will be implemented differently, not needed as-is at the moment.

--- test/test_import.py
+++
@@ -1,6 +0,0 @@
-from archist.model.import_node import ImportNode
-from src.archist.module_scanner import ModuleScanner
-
-
-def test_import_single_module(module_with_one_import_path):
-    assert ModuleScanner().scan_module(module_with_one_import_path).import_nodes == [ImportNode('math')]
