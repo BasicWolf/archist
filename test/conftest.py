2a3963a806c57a98afe5bcd1934ecc91bc031452
Zaur Nasibov <zaur@znasibov.info>
2023-10-24 22:14:17 +0300

    Remove unused fixture: `module_with_one_import_path`

--- test/conftest.py
+++ test/conftest.py
@@ -12,13 +12,6 @@ StrPath = Union[str, Path]
 
 
 @pytest.fixture
-def module_with_one_import_path():
-    path = FIXTURES_PATH / 'modules/a_module_with_one_import.py'
-    assert path.exists()
-    return path
-
-
-@pytest.fixture
 def fake_package(fs):
     def _fake_package(
         root_path: StrPath,
